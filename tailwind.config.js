7d8df7ace95b5736de78dc44647061e0f8864782
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-06 20:01:56 +0300

    feat: integrate NativeWind for styling and remove reset script
    - Added NativeWind and Tailwind CSS dependencies in package.json.
    - Removed the reset-project script as it is no longer needed.
    - Updated tsconfig.json to include nativewind-env.d.ts.
    - Created global CSS file for Tailwind styles.
    - Implemented index.tsx with NativeWind classes for styling.
    - Configured Babel and Metro for NativeWind support.
    - Added Tailwind configuration with custom colors.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const { green } = require('react-native-reanimated/lib/typescript/Colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          100: "#D33B0D"
+        },
+        green: {
+          100: "#084137"
+        }
+     }
+    }
+  },
+  plugins: []
+}

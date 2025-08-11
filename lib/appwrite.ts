02f50ea37a000a534ebc85436b463866d1ef8a0d
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-11 19:00:03 +0300

    feat: add Appwrite configuration and environment variables

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.esraacodes.es-fastfood",
+    projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+};

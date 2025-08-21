fc8906f242a76786672fe73110f12fe38c401298
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-21 18:32:43 +0300

    Add CartItem component and integrate it into carttab

--- app/(tabs)/carttab.tsx
+++ app/(tabs)/carttab.tsx
@@ -1,10 +1,11 @@
-import {FlatList, Text, View} from 'react-native';
-import cn from "clsx";
-import {PaymentInfoStripeProps} from "@/type";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {useCartStore} from "@/store/cart.store";
-import CustomHeader from "@/components/CustomHeader";
+import CartItem from "@/components/CartItem";
 import CustomButtom from "@/components/CustomButtom";
+import CustomHeader from "@/components/CustomHeader";
+import { useCartStore } from "@/store/cart.store";
+import { PaymentInfoStripeProps } from "@/type";
+import cn from "clsx";
+import { FlatList, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const PaymentInfoStripe = ({ label,  value,  labelStyle,  valueStyle, }: PaymentInfoStripeProps) => (
     <View className="flex-between flex-row my-1">
@@ -24,27 +25,30 @@ const carttab = () => {
     <SafeAreaView>
         <FlatList
             data={items}
-            renderItem={() => {
-                return (
-                    <View className="flex-col gap-16">
-                        <View className="border rounded-2xl border-gray-100 px-4 py-8 flex-col gap-5 w-[95%] mx-auto">
-                            <Text className="h3-bold">Payment Summary</Text>
-                            <PaymentInfoStripe labelStyle="base-semibold" valueStyle="base-bold" label={`Total items (${getTotalItems()})`} value={`$${getTotalPrice()}`} />
-                            <PaymentInfoStripe labelStyle="base-semibold" valueStyle="base-bold" label="Delivery Fee" value="Free" />
-                            <PaymentInfoStripe labelStyle="base-semibold" valueStyle="base-bold" label="Discount" value="-$0.5" />
-                            <View className="border-t-2 border-gray-100" />
-                            <PaymentInfoStripe label="Total" value={`$${getTotalPrice() - 0.5}`} />
-                        </View>
-                        <CustomButtom title="Order Now"/>
-                    </View>
-                )
-            }}
+            renderItem={({item}) => (
+                <CartItem item={item} />
+            )}
             keyExtractor={(item) => item.id}
             ListHeaderComponent={() => (
                 <View className="px-6 pt-8">
                     <CustomHeader />
                 </View>
             )}
+            ListFooterComponent={() => {
+                return (
+                    <View className="flex-col gap-16">
+                        <View className="border rounded-2xl border-[#EDEDED] px-4 py-8 flex-col gap-5 w-[90%] mx-auto">
+                            <Text className="h3-bold">Payment Summary</Text>
+                            <PaymentInfoStripe  label={`Total items (${getTotalItems()})`} value={`$${getTotalPrice()}`} />
+                            <PaymentInfoStripe label="Delivery Fee" value="Free" />
+                            <PaymentInfoStripe label="Discount" valueStyle="text-green-700" value="-$0.5" />
+                            <View className="border-t-2 border-[#EDEDED]" />
+                            <PaymentInfoStripe label="Total" value={`$${getTotalPrice() - 0.5}`} />
+                        </View>
+                        <CustomButtom style="w-[90%] mx-auto" textStyle="paragraph-bold" title="Order Now"/>
+                    </View>
+                )
+            }}
             ListEmptyComponent={() => <Text>Cart is Empty</Text>}
         />
     </SafeAreaView>
